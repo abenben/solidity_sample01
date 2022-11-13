64b3a506f7ea677a74140c72905589a1e16941ab
abenben <abenbenben@gmail.com>
2022-11-13 11:36:45 +0900

    修正

--- contracts/SampleCode.sol
+++ contracts/SampleCode.sol
@@ -3,13 +3,13 @@ pragma solidity >=0.4.22 <0.9.0;
 
 contract SampleCode {
 
-    uint v2;
+    uint v3;
 
     function set(uint v_) public {
-        v2 = v_;
+        v3 = v_;
     }
 
     function get() public view returns (uint){
-        return v2;
+        return v3;
     }
 }
